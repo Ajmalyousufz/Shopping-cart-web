faee811f83b6b465e2ee7b8121bc6a8aeb823fff
Ajmalyousufza <ajmalyousufza@gmail.com>
2021-11-05 07:11:46 +0530

    Shopping cart first commit - a user and admin panel started

---
+++ routes/user.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products = [
+    {
+      name : "SAMSUNG GALAXY A1",
+      category : "Phone",
+      discription : "A1 New Advanced Technologies added. 256mp camera.",
+      image : "https://m.media-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg"
+    },
+    {
+      name : "I PHONE 11",
+      category : "Phone",
+      discription : "Iphone 11 New Advanced Technologies added. 256mp camera.",
+      image : "https://m.media-amazon.com/images/I/71DVgBTdyLL._SL1500_.jpg"
+    },
+    {
+      name : "POCO F11",
+      category : "Phone",
+      discription : "F11 New Advanced Technologies added. 256mp camera.",
+      image : "https://m.media-amazon.com/images/I/81-fNmQqlLL._AC_SL1500_.jpg"
+    },
+    {
+      name : "OPP F3",
+      category : "Phone",
+      discription : "F3New Advanced Technologies added. 256mp camera.",
+      image : "https://vlebazaar.in/image/cache/catalog//B074TZG666/OPPO-F3-4GB-Mobile-Phone-Rose-Gold-64GB-B074TZG666-1000x1000h.jpg"
+    }
+  ];
+  res.render('index', { products,admin:false});
+});
+
+module.exports = router;
